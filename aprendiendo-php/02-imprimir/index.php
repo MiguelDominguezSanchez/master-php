c8399efebc7f3b81b148c73776fc9c8daa245155
MiguelDominguezSanchez <0.miguel.dominguez@gmail.com>
2022-04-04 20:37:00 +0200

    Sección 6: Primeros pasos con PHP - 23. Comentarios de código

--- aprendiendo-php/02-imprimir/index.php
+++ aprendiendo-php/02-imprimir/index.php
@@ -5,15 +5,28 @@
     <title>Imprimir por pantalla - Master en PHP</title>
   </head>
   <body>
-    <h1>Master en PHP - victorrroblesweb.es</h1>
+    <h1>Master en PHP - <?php echo "victorrroblesweb.es" ?></h1>
+
+    <?="Bienvenido al curso más grande de PHP"?>
+
     <?php
+        // Titular de la sección
         echo "<h3>Listado de videojuegos:</h3>";
-        echo"<ul>"
+
+        /*
+            Esta es un lista
+            de videojuegos
+            moderno
+        */
+        echo "<ul>"
             ."<li>GTA</li>"
             ."<li>Fifa</li>"
             ."<li>Mario Bros</li>"
             ."</ul>";
+        
+        //echo "<br>HOLA HOLA HOLA<br>";
 
+        // Párrafo explicativo
         echo '<p>Esta es toda'.' - '.'lista de juegos </p>';
     ?>
   </body>
