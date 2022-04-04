e697c5dd1337dc8082705152b597ff959379e5c6
MiguelDominguezSanchez <0.miguel.dominguez@gmail.com>
2022-04-04 21:16:51 +0200

    Sección 7: Variables y tipos de datos - 26. Tipos de datos en PHP

---
+++ aprendiendo-php/04-tipos/index.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+ * TIPOS DE DATOS
+ * Entero (int / integer) = 99
+ * Coma flotante / decimales (float / double) = 3.45
+ * Cadenas de texto (string) = "Hola soy un string"
+ * Booleano (bool) = true false
+ * null
+ * Array (Colección de datos)
+ * Objetos
+ */
+
+$numero = 100;
+$decimal = 27.9;
+$texto = "Soy un texto";
+$verdadero = false;
+$nula = null;
+
+echo gettype($nulla);
+
