4d7fc1c1d1ab0fc6692e4848eecf068b13c1e48e
MiguelDominguezSanchez <0.miguel.dominguez@gmail.com>
2022-04-04 20:56:10 +0200

    Sección 7: Variables y tipos de datos - 25. Más variables

--- aprendiendo-php/03-variables/index.php
+++ aprendiendo-php/03-variables/index.php
@@ -5,7 +5,13 @@
     $numero = 44;
     $verdadero = true;
 
+    $numero = 77;
+
     echo '<h1>'.$mi_primera_variable.'</h1>';
     echo $numero."<br>";
+
+    $numero = 120;
+
+    echo $numero;
 
 ?>
