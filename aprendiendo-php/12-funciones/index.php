22b3ba5e507b25ba5d43f07da6090f6a3cdc5fc0
MiguelDominguezSanchez <0.miguel.dominguez@gmail.com>
2022-04-05 22:05:11 +0200

    Sección 14: Funcioines - 58. Funciones dentro de otras

--- aprendiendo-php/12-funciones/index.php
+++ aprendiendo-php/12-funciones/index.php
@@ -86,14 +86,29 @@ function calculadora($numero1, $numero2, $negrita = false){
 
 }
 
-calculadora(10, 20, true);
+echo calculadora(10, 20, true);
 
-function devuelveElNombre($nombre){
-    return "El nombre es: $nombre";
+// Ejemplo 4
+
+function getNombre($nombre){
+    $texto = "El nombre es: $nombre";
+    return $texto;
 }
 
-echo devuelveElNombre("Victor Robles");
+function getApellidos($apellidos){
+    $texto = "los apellidos son: $apellidos";
+    return $texto;
+}
 
+function devuelveElNombre($nombre, $apellidos){
+    $texto = getNombre($nombre)
+             ."<br/>"
+             .getApellidos($apellidos);
+    return $texto;
+}
+
+echo devuelveElNombre("Victor", "Robles");
+// echo getNombre("Paco");
 
 
 
