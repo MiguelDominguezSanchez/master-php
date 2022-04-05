3a080d76d4d0e897e7689965ec6e8826269738b8
MiguelDominguezSanchez <0.miguel.dominguez@gmail.com>
2022-04-05 11:13:45 +0200

    Sección 11: Estructuras de control - Condicionales - 37. Operadores de comparración

--- aprendiendo-php/08-condicionales/index.php
+++ aprendiendo-php/08-condicionales/index.php
@@ -21,6 +21,7 @@
  * >= mayor o igual que
  * */
 
+// Ejemplo 1
 $color = "verde";
 
 if($color == "rojo") {
@@ -31,9 +32,61 @@ if($color == "rojo") {
 
 echo "<br>";
 
+// Ejemplo 2
 $year =  2019;
 if($year >= 2019){
     echo "Estamos de 2019 en adelante";
 } else {
     echo "Estamos en 2019";
 }
+
+// Ejemplo 3
+
+$nombre = "David Extremadura";
+$ciudad = "Madrid";
+$continente = "Aisa";
+$edad = 49;
+$mayoria_edad = 18;
+
+
+if($edad >= $mayoria_edad) {
+    echo"<h1>$nombre es mayor de edad</h1>";
+
+    if($continente == "Europa"){
+        echo "<h2>Y es de $ciudad</h2>";
+    } else {
+        echo "No es Europeo";
+    }
+
+} else {
+    echo "<h2>$nombre NO es mayor de edad</h2>";
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
