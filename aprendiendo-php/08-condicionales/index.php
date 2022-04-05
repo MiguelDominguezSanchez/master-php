f3d3ef7f3d31084681afeadb6f093295269183cb
MiguelDominguezSanchez <0.miguel.dominguez@gmail.com>
2022-04-05 11:59:50 +0200

    Sección 11: Estructuras de control - Condicionales - 40. Switch

--- aprendiendo-php/08-condicionales/index.php
+++ aprendiendo-php/08-condicionales/index.php
@@ -114,6 +114,32 @@ if($dia == 1) {
 
 echo "<hr/>";
 
+// SWITCH
+$dia = 6;
+
+switch ($dia) {
+    case 1:
+        echo "LUNES";
+        break;
+    case 2:
+        echo "MARTES";
+        break;
+    case 3:
+        echo "MIERCOLES";
+        break;
+    case 4:
+        echo "JUEVES";
+        break;
+    case 5:
+        echo "VIERNES";
+        break;
+    default:
+        echo "ES FIN DE SEMANA";
+
+}
+
+echo "<hr/>";
+
 // Ejemplo 5
 $edad1 = 18;
 $edad = 64;
