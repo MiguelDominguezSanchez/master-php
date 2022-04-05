48df7ffe2bb69a38933b6cbc7416fd214a6cb59f
MiguelDominguezSanchez <0.miguel.dominguez@gmail.com>
2022-04-05 19:29:03 +0200

    Sección 13: Ejercicios de prrogramación - Bloque 1 - 53. Ejercicio 7

---
+++ aprendiendo-php/11-ejercicios/ejercicio7.php
@@ -0,0 +1,30 @@
+<?php
+
+/*
+ * Ejercicio 5. Hacer un progrrama que muestre todos los números IMPARES entre dos números
+ * que nos lleguen por la URL($_GET)
+ */
+
+if(isset($_GET['numero1']) && isset($_GET['numero2'])){
+    $numero1 = $_GET['numero1'];
+    $numero2 = $_GET['numero2'];
+
+    if($numero1 < $numero2) {
+        for($i = $numero1; $i <= $numero2; $i++) {
+
+            if($i%2 != 0) {
+                echo "<h4>$i Es IMPAR</h4>";
+            } else {
+                echo "<h4>$i Es PAR</h4>";
+            }
+
+        }
+    } else {
+        echo "<h1>El número 1 deb ser menor al número 2</h1>";
+    }
+
+
+} else {
+    echo "<h1>Los parámetros get no existen</h1>";
+}
+
