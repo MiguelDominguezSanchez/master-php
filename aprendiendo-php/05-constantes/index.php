f4fab209d4a26f49e6d38bb2048c809a51659b30
MiguelDominguezSanchez <0.miguel.dominguez@gmail.com>
2022-04-05 08:53:40 +0200

    Sección 8: Constantes - 28. Constantes

---
+++ aprendiendo-php/05-constantes/index.php
@@ -0,0 +1,15 @@
+<?php
+
+// Constantes
+// Es un contenedor de información que no puede variar
+
+define('nombre','Victor Robles');
+define('web', 'victorrobles.es');
+
+echo "<h1>".nombre."</h1>";
+echo "<h1>".web."</h1>";
+
+// Variable
+$web = "victorroblesweb.es/academy";
+$web = "victorroblesweb.es/cursos";
+echo "<h1>".$web."</h1>";
