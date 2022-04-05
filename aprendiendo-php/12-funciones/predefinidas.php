ab0a77ff1ff9a631378f2f09eaaefcae69db42c4
MiguelDominguezSanchez <0.miguel.dominguez@gmail.com>
2022-04-05 22:47:34 +0200

    Sección 14: Funcioines - 61. Funciones prredefinidas (primera parte)

---
+++ aprendiendo-php/12-funciones/predefinidas.php
@@ -0,0 +1,22 @@
+<?php
+
+$nombre = "Victor Robles";
+var_dump($nombre);
+
+// Fecha
+echo date('d-m-y');
+echo "<br/>";
+echo time();
+
+// Matemáticas
+echo "<br/>";
+echo "Raíz cuadrada de 10: ".sqrt(10);
+
+echo "<br/>";
+echo "Numero aleatorio entre 10 y 40: ".rand(10, 40);
+
+echo "<br/>";
+echo "Numero pi".pi();
+
+echo "<br/>";
+echo "Redondear: ".round(7.89182, 1);
