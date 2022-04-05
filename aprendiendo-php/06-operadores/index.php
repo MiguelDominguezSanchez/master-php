e1abb20e85d625b8b5e6910ec37202ce6697a67e
MiguelDominguezSanchez <0.miguel.dominguez@gmail.com>
2022-04-05 09:08:13 +0200

    Sección 9: Operadores - 30. Operadores arittméticos

---
+++ aprendiendo-php/06-operadores/index.php
@@ -0,0 +1,13 @@
+<?php
+
+// operadores aritméticos
+$numero1 = 65;
+$numero2 = 33;
+
+echo 'Suma: '.($numero1+$numero2).'<br>';
+echo 'Resta: '.($numero1-$numero2).'<br>';
+echo 'Multiplicación: '.($numero1*$numero2).'<br>';
+echo 'Diivisión: '.($numero1/$numero2).'<br>';
+echo 'Resto: '.($numero1%$numero2).'<br>';
+
+// Operadores incermento y decrremento
