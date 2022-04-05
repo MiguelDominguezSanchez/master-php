d1212adba7cc29fef1a5ae951114632a3d95c112
MiguelDominguezSanchez <0.miguel.dominguez@gmail.com>
2022-04-05 22:29:58 +0200

    Sección 14: Funcioines - 60. Funciones variables

--- aprendiendo-php/12-funciones/variables.php
+++ aprendiendo-php/12-funciones/variables.php
@@ -24,3 +24,53 @@ function holaMundo(){
 }
 
 echo holaMundo();
+
+// Funciones variables
+
+function buenasDias(){
+    return "<h1>Hola! Buenos días :)</h1>";
+}
+
+function buenasTardes(){
+    return "<h1>Hey! Que tal ha ido la comida??</h1>";
+}
+
+function buenasNoches(){
+    return "<h1>¿Te vas a dormir ya? Buenas noches!!</h1>";
+}
+
+$horario = $_GET['horario'];
+
+$miFuncion = "buenas".$horario;
+echo $miFuncion();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
