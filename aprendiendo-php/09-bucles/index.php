131f5c98d025501207d0de1586d2c638997ce934
MiguelDominguezSanchez <0.miguel.dominguez@gmail.com>
2022-04-05 12:50:47 +0200

    Sección 12: Estructuras de control - Iteradores y bucles - 43. Ejemplos con While

--- aprendiendo-php/09-bucles/index.php
+++ aprendiendo-php/09-bucles/index.php
@@ -22,4 +22,53 @@ while($numero <= 100){
     }
 
     $numero++;
+
 }
+
+echo "<hr/>";
+
+// Ejemplo
+
+if(isset($_GET['numero'])){
+    // Cambiar tipo de datos
+    $numero = (int)$_GET['numero'];
+} else {
+    $numero = 1;
+}
+
+echo "<h1>Tabla de multiplicar del numero $numero</h1>";
+
+$contador = 0;
+while($contador <= 10){
+    echo "$numero x $contador = ".($numero*$contador)."<br/>";
+    $contador++;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
