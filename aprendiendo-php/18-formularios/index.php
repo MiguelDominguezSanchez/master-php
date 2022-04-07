32d97e041649358eca35495f9b27b9c9a51d4e93
MiguelDominguezSanchez <0.miguel.dominguez@gmail.com>
2022-04-07 19:39:47 +0200

    Sección 20: Formularios - 79. Tipos de campos

--- aprendiendo-php/18-formularios/index.php
+++ aprendiendo-php/18-formularios/index.php
@@ -13,8 +13,75 @@
     <p>
       <input type="text" name="nombre" />
     </p>
+
     <label for="apellido">Apellido:</label>
     <p><input type="text" name="apellido" /></p>
+
+    <label for="boton">Botón:</label>
+    <p><input type="button" name="boton" value="cliclame" /></p>
+
+    <label for="sexo">Sexo:</label>
+    <p>
+      Hombre: <input type="checkbox" name="sexo" value="hombre" />
+      Mujer: <input type="checkbox" name="sexo" value="mujer" checked="checked" />
+    </p>
+
+    <label for="color">Color:</label>
+    <p><input type="color" name="color" /></p>
+
+    <label for="fecha">Fecha:</label>
+    <p><input type="date" name="apellido" /></p>
+
+    <label for="numero">Numero:</label>
+    <p><input type="number" name="numero" /></p>
+
+    <label for="pass">Contraseña:</label>
+    <p><input type="password" name="pass" /></p>
+
+    <label for="continente">Continente:</label>
+    <p>
+      America del sur <input type="radio" name="continente" value="America del sur" />
+      Europa <input type="radio" name="continente" value="Europa" />
+      Asia <input type="radio" name="continente" value="Asia" />
+    </p>
+
+    <label for="web">Pagina web:</label>
+    <p><input type="url" name="web" /></p>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     <input type="submit" value="Enviar" />
   </form>
 </body>
