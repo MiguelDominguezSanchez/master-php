aea67748d680034bea849548fa22d94fe71ee41d
MiguelDominguezSanchez <0.miguel.dominguez@gmail.com>
2022-04-07 09:29:10 +0200

    Includes 16: Arrays y Arreglos - 69. Funciones para arrays

---
+++ aprendiendo-php/14-arrays/funciones.php
@@ -0,0 +1,67 @@
+<?php
+
+$cantantes = ['2pac','Drake', 'Zwei Millionen','Jennifer Lopez','Alfredo'];
+$numeros = [1,2,5,8,3,4];
+
+// Ordenar
+sort($numeros);
+var_dump($numeros);
+
+// Añadir elementos array
+$cantantes[] = "Natos";
+array_push($cantantes, "Waor");
+
+// Eliminar elementos array
+array_pop($cantantes);
+unset($cantantes[2]);
+
+// Aleatorio
+$indice = array_rand($cantantes);
+echo $cantantes[$indice];
+
+// Dar la vuelta
+var_dump(array_reverse($numeros));
+
+// Buscar dentro de un arrray
+$resultado = array_search('Alfredo', $cantantes);
+var_dump($resultado);
+
+// Contar número de elementos
+echo sizeof($cantantes);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
